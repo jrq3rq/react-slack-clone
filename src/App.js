fe44cc10298bc834ac2504699db35c6493817ce3
YOUR NAME <YOUR EMAIL ADDRESS>
2020-08-11 18:34:12 -0700

    updates added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  // BEM naming convention
+  const [{ user }, dispatch] = useStateValue();
+
   return (
-    <div className="App">
-      <h1>Slack-Clone</h1>
-      {/* header  */}
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* react-router -> chat screen */}
-      </div>
+    <div className="app">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome!</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,32 @@
 import React from "react";
-import "./Header.css";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import "./Header.css";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ user }] = useStateValue();
+
   return (
     <div className="header">
       <div className="header__left">
         <Avatar
           className="header__avatar"
-          // alt={user?.displayName}
-          alt="James Bond"
-          // src={user?.photoURL}
-          src=""
+          alt={user?.displayName}
+          src={user?.photoURL}
         />
         <AccessTimeIcon />
-        {/* avatar for logged in user */}
-        {/* time icon */}
       </div>
+
       <div className="header__search">
         <SearchIcon />
-        {/* seach icon */}
-        <input placeholder="Search J programs" />
-        {/* input */}
+        <input placeholder="Search" />
       </div>
+
       <div className="header__right">
         <HelpOutlineIcon />
-        {/* healp icon */}
       </div>
     </div>
   );
